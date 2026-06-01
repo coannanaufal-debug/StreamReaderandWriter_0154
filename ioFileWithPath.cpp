5327751909657a2b4d31d88e78c54fef793806d2
coannanaufal-debug <coanna.naufal@gmail.com>
2026-06-01 15:08:52 +0700

    tambah logika untuk membaca dan menampilkan isi file setelah membukanya

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -39,3 +39,11 @@ int main()
     infile.open(NamaFile + ".txt", ios::in);
 
     cout << endl << ">= Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk menampilkan setiap baris
+        while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
