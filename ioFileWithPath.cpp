830bc0a426edca605c67ef2417fd4ed43ebe1b2e
coannanaufal-debug <coanna.naufal@gmail.com>
2026-06-01 15:08:13 +0700

    tambah logika untuk menutup file setelah menulis dan membuka file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,3 +30,8 @@ int main()
         outfile << baris << endl;
 
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
